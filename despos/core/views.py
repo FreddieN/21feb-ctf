18320370083f3aa9071155b0b22ed508ef50c7cf
FreddieN <fgn21@ic.ac.uk>
2024-02-20 19:20:46 +0000

    cool

--- despos/core/views.py
+++ despos/core/views.py
@@ -6,8 +6,10 @@ from django.http import HttpResponse
 import datetime
 from django.views.generic import View
 from .models import ProductCategory, Product, Cart, CartEntry
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def index(request):
     context = {}
     context["products"] = {}
@@ -22,21 +24,21 @@ def index(request):
     for productCategory in productCategories.iterator():
         context["products"][productCategory.title] = Product.objects.filter(category=productCategory)
     return render(request, 'pos.html', context)
-
+@login_required
 def receipt(request):
     context = {}
     
     return render(request, 'receipt.html', context)
-
+@login_required
 def void(request):
     context = {}
     
     return redirect('/')
-
+@login_required
 def checkout(request):
     
     return redirect('/')
-
+@login_required
 def addProduct(request, product_id):
     cart, created = Cart.objects.get_or_create(user=request.user)
     itemToAdd = Product.objects.get(pk=product_id)
